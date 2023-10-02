b40eefaf128dfe7fe96cf500edd8b9e4829f47be
harika thallam <hthallam@iu.edu>
2023-10-01 21:16:25 -0400

    Google OAuth int - client

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,5 +1,6 @@
 import '../App.css';
 import { Link } from 'react-router-dom';
+import getGoogleOAuthURL from '../utils/getGoogleUrl';
 
 function LoginPage() {
     return (
@@ -66,6 +67,17 @@ function LoginPage() {
                                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg w-full"
                             >
                                 Log In
+                            </button>
+                        </div>
+                        <div className="mb-6">
+                            <button
+                                type="submit"
+                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg w-full"
+                            >
+                                <a href={getGoogleOAuthURL()}>
+                                Use Google to log in
+                                </a>
+                                
                             </button>
                         </div>
                     </form>
