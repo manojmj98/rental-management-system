89a22d64b4dc25237287bf8ed8504fb352da1763
Manoj Kumar <jalav@iu.edu>
2023-10-15 16:28:12 -0400

    getproducts, create product completed

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,9 @@ import ForgotPage from './components/ForgotPage';
 import AdminPage from './components/AdminPage'
 import RobotListingPage from './components/store/ProductListPage';
 import RobotDetailsPage from './components/store/ProductItemPage';
+import OwnerDashBoard from './components/ownerDashboard/ownerDashBoard';
+import ProductInput from './components/ownerDashboard/ProductInput';
+import Productpage from './components/ownerDashboard/Productpage';
 
 function App() {
   return (
@@ -34,6 +37,10 @@ function App() {
           <Route path="/profile" element={<ProfilePage />} />
           <Route path="/reset" element={<ResetPage />} />
         </Route>
+
+        <Route path="/owner" element={<OwnerDashBoard />} />
+        <Route path="/robotInput" element={<ProductInput />} />
+        <Route path="/productpage/:id" element={<Productpage />} />
 
         {/* Admin Routes */}
         <Route path='' element={<AdminRoute />}>
---
+++ client/src/components/ownerDashboard/ownerDashBoard.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import logo from "../../logo.svg";
+import Card from "../common/Card";
+import NavBar from "../common/NavBar";
+import { FaPlus } from 'react-icons/fa';
+import { Link } from "react-router-dom";
+import { useGetproductsMutation } from "../../slices/userApiSlice";
+import Productpage from "./Productpage";
+import { useSelector } from "react-redux/es/hooks/useSelector";
+function OwnerDashBoard(props) {
+const [robots, setrobots] = useState(null)
+
+const [getProducts, { data }] = useGetproductsMutation(); 
+
+React.useEffect(() => {
+  getProducts();
+}, []);
+
+React.useEffect(() => {
+  if (data) {
+    setrobots(data.products);
+  }
+}, [data]);
+var filterdrobots = null;
+const userInfo = useSelector(state => state.auth.userInfo.id);
+if(robots){
+    
+    filterdrobots = robots.filter((robot) => robot.owner === userInfo);
+}
+  return (
+    <>
+      <NavBar />
+      <div className="flex bg-white ">
+        <div className="flex flex-col items-center w-1/5 pt-1/5 h-screen pt-3/12 ">
+          <img src={logo}></img>
+          <button className="bg-white text-gray-900 hover:bg-blue-700 w-full py-2 px-4 rounded">
+            My Ads
+          </button>
+          <button className="bg-white text-gray-900 hover:bg-blue-700 w-full py-2 px-4 rounded">
+            My Bookings
+          </button>
+          <button className="bg-white text-gray-900 hover:bg-blue-700 w-full py-2 px-4 rounded">
+            Transactions
+          </button>
+          <button className="bg-white text-gray-900 hover:bg-blue-700 w-full py-2 px-4 rounded">
+            Reports
+          </button>
+          <button className="bg-white text-gray-900 hover:bg-blue-700 w-full py-2 px-4 rounded">
+            Settings
+          </button>
+        </div>
+        <section className="container mx-auto flex flex-col items-center mt-4">
+        <div className="flex items-center space-x-2 pb-4">
+  <FaPlus className="text-green-300 text-2xl" />
+  <Link to={`/robotInput`} className="text-blue-500 font-semibold hover:underline">
+    Add a Robot Listing
+  </Link>
+</div>
+    <div className="grid grid-cols-3 gap-6 w-full">
+          { 
+          filterdrobots && filterdrobots.map((robot) => (
+        <Card
+            key={robot._id}
+            title={robot.name}
+            description={robot.description}
+            price ={robot.price}
+          />  
+            ))}
+          </div>
+        </section>
+        
+    </div>
+    </>
+  );
+}
+
+export default OwnerDashBoard;
