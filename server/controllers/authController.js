eb12e13064efad33f327a48646ff837a923c7fd8
harika thallam <hthallam@iu.edu>
2023-10-16 16:17:18 -0400

    Clean up to match the main

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -447,6 +447,53 @@ const generateToken = (res, userId) => {
         maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
     });
 };
+const googleOauthHandler = async(req,res) =>{
+    const googleUser = jwt.decode(req.body.credential)
+  
+    if (!googleUser.email_verified) {
+      return res.status(403).send("Google account is not verified");
+    }
+  
+    //upsert the user
+    const user = await User.findOneAndUpdate(
+      {
+        email: googleUser.email,
+      },
+      {
+        email: googleUser.email,
+        name: googleUser.name,
+        picture: googleUser.picture, // TODO: Add picture if it is not already part of the user doc model
+        username: googleUser.given_name
+      },
+      {
+        upsert: true,
+        new: true,
+      }
+    );
+  
+    const token = jwt.sign({
+      email: user.email,
+      id: user._id
+    },process.env.JWT_SECRET,{expiresIn:"4h"})
+  
+  res.cookie('jwt', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== 'development',
+      sameSite: 'strict',
+      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+  });
+  
+  res.status(200).json({
+    success: true,
+    user: {
+        id: user._id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        role: user.role
+    }
+  })
+};
 
 module.exports = {
     registerUser,
@@ -458,4 +505,5 @@ module.exports = {
     createQuestions,
     verifyQuestions,
     getQuestions,
+    googleOauthHandler
 }
