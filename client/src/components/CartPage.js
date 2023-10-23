bb6a9f4184e529f61c4757bb75655b95fa5f569c
Aidan Rosberg <arosberg@iu.edu>
2023-10-22 23:03:24 -0400

    Added checkout button and total

--- client/src/components/CartPage.js
+++ client/src/components/CartPage.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router';
 import NavBar from './common/NavBar';
-import Card from './common/Card';
-import { Link } from 'react-router-dom';
 import CartCard from './common/CartCard';
 import { addToCart, removeFromCart } from '../slices/cartSlice';
 
 function CartPage() {
-  const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const { cartItems } = useSelector((state) => state.cart);
-  
+  const { cartItems, itemsPrice } = useSelector((state) => state.cart);
+
   const addToCartHandler = (product, qty) => {
     dispatch(addToCart({ ...product, qty }));
   };
@@ -21,26 +17,40 @@ function CartPage() {
     dispatch(removeFromCart(id));
   };
 
+  // Add functionality when available
+  const checkoutHandler = () => {};
+
   return (
     <div className='bg-white h-screen flex flex-col'>
       {/* Navbar */}
-      <NavBar className='bg-white' />
+      <NavBar />
 
-      {/* Robot Listing Section */}
-      <section className='container mx-auto flex flex-col items-center mt-8'>
+      <section className='container mx-auto flex flex-col mt-8'>
         <h3 className='text-3xl font-semibold text-black mb-6'>
           Your Shopping Cart
         </h3>
         <div className='grid grid-cols-1 gap-6'>
           {cartItems &&
             cartItems.map((robot) => (
-                <CartCard
-                  key={robot._id}
-                  robot={robot}
-                  removeFromCart={removeFromCartHandler}
-                  addToCart={addToCartHandler}
-                />
+              <CartCard
+                key={robot._id}
+                robot={robot}
+                removeFromCart={removeFromCartHandler}
+                addToCart={addToCartHandler}
+              />
             ))}
+        </div>
+        <div className='grid grid-cols-1 justify-end justify-items-end'>
+          <h3 className='text-3xl font-semibold text-black mb-3'>
+            Total: {itemsPrice}
+          </h3>
+          <button
+            className='bg-blue-500 w-min hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg w-full disabled:bg-blue-300'
+            disabled={cartItems.length === 0}
+            onClick={checkoutHandler}
+          >
+            Checkout
+          </button>
         </div>
       </section>
 
